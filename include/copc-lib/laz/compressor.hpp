a4f45c7ba5090d19431390a72cdb2997a5d2ca27
Christopher Lee <csl170000@utdallas.edu>
2021-09-06 15:19:24 -0500

    expose more compressor functions

--- include/copc-lib/laz/compressor.hpp
+++ include/copc-lib/laz/compressor.hpp
@@ -17,13 +17,14 @@ class Compressor
 {
   public:
     // Compresses bytes and writes them to the out stream
-    static uint32_t CompressBytes(std::ostream &out_stream, las::LasHeader const &header, std::vector<char> &in)
+    static uint32_t CompressBytes(std::ostream &out_stream, uint16_t point_format_id, uint16_t eb_count,
+                                  uint16_t point_record_length, std::vector<char> &in)
     {
         OutFileStream stream(out_stream);
 
-        las_compressor::ptr compressor = build_las_compressor(stream.cb(), header.point_format_id, header.ebCount());
+        las_compressor::ptr compressor = build_las_compressor(stream.cb(), point_format_id, eb_count);
 
-        int point_size = header.point_record_length;
+        int point_size = point_record_length;
         if (in.size() % point_size != 0)
             throw std::runtime_error("Invalid input stream for compression!");
 
@@ -37,6 +38,19 @@ class Compressor
         }
         compressor->done();
         return point_count;
+    }
+
+    static uint32_t CompressBytes(std::ostream& out_stream, las::LasHeader const& header, std::vector<char>& in) {
+        return CompressBytes(out_stream, header.point_format_id, header.ebCount(), header.point_record_length, in);
+        }
+
+    static std::vector<char> CompressBytes(std::vector<char> &in, uint16_t point_format_id, uint16_t eb_count,
+                                               uint16_t point_record_length)
+    {
+        std::ostringstream out_stream;
+        CompressBytes(out_stream, point_format_id, eb_count, point_record_length, in);
+        auto ostr = out_stream.str();
+        return std::vector<char>(ostr.begin(), ostr.end());
     }
 };
 } // namespace copc::laz
