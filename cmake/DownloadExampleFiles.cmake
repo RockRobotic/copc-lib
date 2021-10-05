9c4400e23e4abe255c84b28060f255e68b5ea837
Christopher Lee <csl170000@utdallas.edu>
2021-10-04 21:25:51 -0500

    add updated autzen test file temporarily

--- cmake/DownloadExampleFiles.cmake
+++ cmake/DownloadExampleFiles.cmake
@@ -2,6 +2,7 @@ macro(download_test_files OUT_PATH)
 
 	if(NOT EXISTS ${OUT_PATH})
 		if (EXISTS ${BaseAutzenFilePath})
+			file(WRITE ${OUT_PATH}) # this will create any necessary pathes, as CREATE_LINK doens't
 			file(CREATE_LINK ${BaseAutzenFilePath} ${OUT_PATH})
 		else()
 			message(STATUS "Downloading test files to ${OUT_PATH}")
