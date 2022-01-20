e132c7c1973c1e3e8ff6847f0b10b18817f06fe9
Christopher Lee <csl170000@utdallas.edu>
2022-01-20 17:56:51 -0600

    test

--- test/utils.py
+++ test/utils.py
@@ -10,10 +10,3 @@ def get_autzen_file():
 
 def get_data_dir():
     return DATADIRECTORY
-
-
-def add_to_python_path():
-    DIR = os.path.dirname(os.getcwd())
-    ROOT = os.path.join(DIR, "..")
-    sys.path.insert(0, ROOT)
-    sys.path.insert(0, DIR)
