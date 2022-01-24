bddf734592658bdb268f2e64ac5502e5a41bca1b
Christopher Lee <csl170000@utdallas.edu>
2022-01-24 14:04:30 -0600

    Add readme to pypi description

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ with open("version", "r") as file:
     lines = file.readlines()
     version = lines[0]
 
+with open("README.md", "r", encoding="utf-8") as fp:
+    readme = fp.read()
+
 from setuptools import find_packages
 
 setup(
@@ -17,6 +20,8 @@ setup(
     author_email="support@rockrobotic.com",
     maintainer="Rock Robotic Inc.",
     maintainer_email="support@rockrobotic.com",
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url="https://github.com/RockRobotic/copc-lib",
     extras_require={"test": ["pytest"]},
     cmake_args=["-DWITH_PYTHON:BOOL=ON", "-DWITH_TESTS=OFF", "-DEMSCRIPTEN=OFF"],
@@ -24,4 +29,16 @@ setup(
     packages=find_packages(where="python"),
     package_dir={"": "python"},
     exclude_package_data={"": ["libs/laz-perf"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: GIS",
+    ],
 )
