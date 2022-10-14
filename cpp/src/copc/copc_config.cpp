81b2daaeceb4da9f8d2804110b278bc41ee73226
Leo Stanislas <leo.stanislas@gmail.com>
2022-10-14 16:35:58 -0400

    Remove the need for a LazConfigWriter in LazWriter (#133)

--- cpp/src/copc/copc_config.cpp
+++ cpp/src/copc/copc_config.cpp
@@ -17,7 +17,6 @@ CopcConfigWriter::CopcConfigWriter(const int8_t &point_format_id, const Vector3 
                                    const std::string &wkt, const las::EbVlr &extra_bytes_vlr, bool has_extended_stats)
     : CopcConfig(point_format_id, scale, offset, wkt, extra_bytes_vlr, has_extended_stats)
 {
-    LasHeader()->IsCopc(true);
     if (point_format_id < 6 || point_format_id > 8)
         throw std::runtime_error("LasConfig: Supported point formats are 6 to 8.");
 }
